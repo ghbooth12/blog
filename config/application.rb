18c0c64080bdf76c106cf72445c043ad076d2d3b
Gahee Heo <ghbooth12@gmail.com>
2016-12-13 15:46:15 -0600

    2.4 Class Autoloading

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,9 @@ require 'simplemvc'
 
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
-require 'my_pages_controller'
+# No need to require controllers here.
+# Because we'll use const_missing method to automatically require files with needed classes.
+# require 'my_pages_controller'
 
 module Blog
   class Application < Simplemvc::Application # Blog Application inherits from Simplemvc Application so that is we can run Blog::Application.
