707c20a8e867fe472ed80a0e92aa542e126f5ec5
Gahee Heo <ghbooth12@gmail.com>
2016-12-14 12:08:05 -0600

    2.5 Rack Request and Response: locals section

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,14 +1,18 @@
 class MyPagesController < Simplemvc::Controller
+  # attr_reader :name  # I think I don't need this.
+
   def about
     render :about, name: "Gahee", last_name: "Booth"  # This is response.
   end
 
   def tell_me
-    puts "Before render"
-    render :tell_me, name: params["name"]
-    puts "After render" # <-- Error occurs!
+    # puts "Before render"
+    # render :tell_me, name: params["name"]
+    # puts "After render" # <-- Error occurs!
     # Because render() line has to be last line so controller responds properly.
     # So Simplemvc created Rack::Response.new() in Controller class.
     # get_response in simplemvc.rb will return response properly, even if "render" isn't the last line.
+
+    @name = "Apple Baby"
   end
 end
